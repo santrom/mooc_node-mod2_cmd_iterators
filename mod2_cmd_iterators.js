face9a68a0af61bd193a5b5a9f058971d3e50147
santrom <50862120+santrom@users.noreply.github.com>
2019-05-26 12:19:53 +0200

    Version Trabajo

--- mod2_cmd_iterators.js
+++ mod2_cmd_iterators.js
@@ -1,1 +1,23 @@
-// Type your code here
+// Primer apartado
+console.log();
+console.log("Route	to	node.js: " + process.argv[0]); // Muestra ruta del interprete
+console.log("Route	to	this	file: " + process.argv[1]); // Muestra fichero nombre del fichero .js
+console.log();
+
+// Segundo apartado
+let [a,b,...arrayParametros];
+//for arrayParametros.forEach(elemento,i < arrayParamentros.length, i++) => {
+for arrayParametros.forEach(elemento) {
+		if (elemento == "-r") {
+			
+console.log("Existe elemento");
+		}
+}
+
+/*
+array_0.forEach((elem, i) => {
+  if (elem==="-r") { 
+    array_0.splice(i,2);
+  }
+});
+*/
