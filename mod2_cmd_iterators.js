c6cb85cf4c9fb1b449963f11c9de3bc31c05315f
santrom <50862120+santrom@users.noreply.github.com>
2019-05-26 22:11:06 +0200

    Version Trabajo

--- mod2_cmd_iterators.js
+++ mod2_cmd_iterators.js
@@ -1,23 +1,68 @@
-// Primer apartado
+/* Primer apartado */
 console.log();
 console.log("Route	to	node.js: " + process.argv[0]); // Muestra ruta del interprete
 console.log("Route	to	this	file: " + process.argv[1]); // Muestra fichero nombre del fichero .js
 console.log();
 
-// Segundo apartado
-let [a,b,...arrayParametros];
-//for arrayParametros.forEach(elemento,i < arrayParamentros.length, i++) => {
-for arrayParametros.forEach(elemento) {
-		if (elemento == "-r") {
-			
-console.log("Existe elemento");
-		}
-}
-
-/*
-array_0.forEach((elem, i) => {
-  if (elem==="-r") { 
-    array_0.splice(i,2);
-  }
+/* Segundo apartado */
+// Definimos arrayParametros con los parámetros de entrada
+let [,,...arrayParametros] = process.argv;
+// Definimos arrayBorrar que contendrá los elementos que tenemos que borrar del arrayParametros
+let arrayBorrar = [];
+// Detectamos parametro a borrar del arrayParametros y los cargamos en el arrayBorrar
+arrayParametros.forEach(function(elemento,i) {
+	if (elemento == "-r") {
+		arrayBorrar.push(arrayParametros[i+1]); // El parámetro a borrar es el siguiente al parámetro "-r"
+	}	
 });
-*/
+
+
+
+arrayParametros.forEach(function(elemento,i) {
+console.log("ArrayParametros: " + arrayParametros[i]);
+});
+
+
+
+// Añadimos el elemento "-r" al arrayBorrar
+arrayBorrar.push('-r');
+
+console.log("\n");
+
+arrayBorrar.forEach(function(elemento,i) {
+console.log("ArrayBorrar: " + arrayBorrar[i]);
+});
+
+console.log("\n");
+//
+let arrayLimpio = [];
+
+// Incluimos en arrayLimpio todos los elementos de arrayParametros no incluidos en arrayBorrar
+arrayParametros.forEach(function(elemento,i) {
+//	if (!(arrayParametros[i] in arrayBorrar)) {
+	if (!(arrayBorrar.includes(arrayParametros[i]))) {
+		arrayLimpio.push(arrayParametros[i]);
+	}
+});
+
+
+arrayLimpio.forEach(function(elemento,i) {
+console.log("ArrayLimpio: " + arrayLimpio[i]);
+});
+
+// Ordenamos alfabeticamente arrayLimpio
+arrayLimpio.sort();
+
+console.log("\n");
+
+
+// Visualizamos los parámetros, contando su número
+
+arrayLimpio.forEach(function(elemento, i) {
+  let cont = arrayLimpio.reduce((ac, el, i, a) => el===elemento ? ++ac : ac, 0);
+  console.log(`${elemento} : ${cont}`);
+});
+
+
+
+
